92911fb8dee891e4d65bc6e00d0cf2365dbaa628
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-05-20 23:28:25 -0500

    Before fixing armed v fired missiles

--- 2d_versions/2d_ml_versions/scrap.py
+++ 2d_versions/2d_ml_versions/scrap.py
@@ -1,21 +1,14 @@
-list = []
-results = []
+def distance_reward(reward, defense_missiles):
+    payment_ponderations = [1, 0.9, 0.85, 0.75, 0.65, 0.55, 0.35, 0.25, 0.15, 0.05]
+    max_index = len(payment_ponderations) - 1
 
-list.append(-10)
-payments = [1, 0.9, 0.85, 0.75, 0.65, 0.55, 0.35, 0.25, 0.15, 0.05]
-
-length = len(list)
-
-for i in range(length):
-    if list[i] > 0:
-        if i < len(payments):
-            results.append(list[i] * payments[i])
-        else:
-            results.append(list[i] * 0)
-    else:
-        results.append(list[i])
+    for i, missile in enumerate(defense_missiles):
+        missile.reward_diff = missile.start_distance - missile.defense_missile_to_attack_missile
+        if missile.reward_diff > 0:
+            ponderation = payment_ponderations[i] if i <= max_index else 0
+            missile.reward_diff *= ponderation
+        reward += missile.reward_diff
     
+    return reward
 
-print("list", list)
-print(results)
 
