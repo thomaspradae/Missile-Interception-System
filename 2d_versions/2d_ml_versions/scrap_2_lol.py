5b11fa028d223a69de2acb2f82db602285100c1e
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-05-22 23:25:12 -0500

    Getting there

--- 2d_versions/2d_ml_versions/scrap_2_lol.py
+++ 2d_versions/2d_ml_versions/scrap_2_lol.py
@@ -1,42 +1,12 @@
 import numpy as np
-global_time = 0
-reward = 0
-target_coordinates = [0, 0]
-defense_coordinates = [0, 0]
-attack_coordinates = [0, 0]
-attack_missile_coordinates = [0, 0]
-attack_missile_theta = 0
-attack_missile_to_target = 0
 
-armed_missiles = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-fired_missiles = []
+carlos = 1
+
+if carlos:
+    print('Carlos')
+
+else:
+    print('Not Carlos')
 
 
-base_state = np.array(
-            [
-                global_time,
-                reward,
-                target_coordinates[0],
-                target_coordinates[1],
-                defense_coordinates[0],
-                defense_coordinates[1],
-                attack_coordinates[0],
-                attack_coordinates[1],
-                attack_missile_coordinates[0],
-                attack_missile_coordinates[1],
-                attack_missile_theta,
-                attack_missile_to_target,
-            ]
-        )
-
-# for missile in armed_missiles:
-#     print("Hello")
-
-
-# for i, missile in enumerate(armed_missiles):
-#     missile_state = np.array(
-
-#     )
-# m
-
-print(True)
+print(np.random.randint(2))
