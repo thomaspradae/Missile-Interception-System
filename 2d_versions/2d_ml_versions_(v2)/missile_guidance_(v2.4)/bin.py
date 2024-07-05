8a9697c929d30ff96060b992a9f31b7d5a5e8123
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-07-05 03:18:05 -0500

    become undeniable

--- 2d_versions/2d_ml_versions_(v2)/missile_guidance_(v2.4)/bin.py
+++ 2d_versions/2d_ml_versions_(v2)/missile_guidance_(v2.4)/bin.py
@@ -1,6 +1,32 @@
-import numpy as np
+import math
+reward = 0
+theta = 0.05 
+missile_angle = 0.05
+theta_deg = math.degrees(theta)
+missile_angle_deg = math.degrees(missile_angle)
+print("theta: ", theta)
+print("missile_angle: ", missile_angle)
+print("theta deg: ", theta_deg)
+print("missile_angle deg: ", missile_angle_deg)
 
-log_number = np.array([[-0.6251854, 0.449011, -0.09058942, -0.3805059, 0.8405625, -0.5417145, 0.65479505, -0.7558065, 0.1857675, 0.21409199, 0.9468592, 0.24846807, 12.423404]])
+angle_diff = abs(theta - missile_angle)
+angle_diff_deg = math.degrees(angle_diff)
+print("angle_diff: ", angle_diff)
+print("angle_diff deg: ", angle_diff_deg)
+print("2*math.pi - angle_diff: ", 2*math.pi - angle_diff)
+angle_diff = min(angle_diff, 2*math.pi - angle_diff)
+angle_diff_deg = math.degrees(angle_diff)
 
-print(np.array([log_number[0][0], log_number[0][1]]))
-
+if angle_diff < 0.05:
+    try:
+        if 1 / angle_diff < 100:
+            reward += 1 / angle_diff
+        else:
+            100
+    except ZeroDivisionError:
+        reward += 100
+    print("reward: ", reward)
+else:
+    reward -= (2 * angle_diff)
+    print("0.1 * angle_diff: ", 0.1 * angle_diff)
+    print("reward: ", reward)
