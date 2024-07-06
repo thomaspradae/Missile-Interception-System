df2d94d7e4a32c7f7e1bec4107b6ba9b39066985
Thomas <92346180+thomaspradae@users.noreply.github.com>
2024-07-05 22:44:12 -0500

    you're not noticing, notice more, this is absurd

--- 2d_versions/2d_ml_versions_(v2)/missile_guidance_(v2.4)/bin.py
+++ 2d_versions/2d_ml_versions_(v2)/missile_guidance_(v2.4)/bin.py
@@ -1,32 +1,9 @@
-import math
-reward = 0
-theta = 0.05 
-missile_angle = 0.05
-theta_deg = math.degrees(theta)
-missile_angle_deg = math.degrees(missile_angle)
-print("theta: ", theta)
-print("missile_angle: ", missile_angle)
-print("theta deg: ", theta_deg)
-print("missile_angle deg: ", missile_angle_deg)
+import numpy as np
+distance = 0.001
 
-angle_diff = abs(theta - missile_angle)
-angle_diff_deg = math.degrees(angle_diff)
-print("angle_diff: ", angle_diff)
-print("angle_diff deg: ", angle_diff_deg)
-print("2*math.pi - angle_diff: ", 2*math.pi - angle_diff)
-angle_diff = min(angle_diff, 2*math.pi - angle_diff)
-angle_diff_deg = math.degrees(angle_diff)
+# generate 10 random numbers between 0 and pi
+angles = np.random.uniform(0, np.pi, 10)
 
-if angle_diff < 0.05:
-    try:
-        if 1 / angle_diff < 100:
-            reward += 1 / angle_diff
-        else:
-            100
-    except ZeroDivisionError:
-        reward += 100
-    print("reward: ", reward)
-else:
-    reward -= (2 * angle_diff)
-    print("0.1 * angle_diff: ", 0.1 * angle_diff)
-    print("reward: ", reward)
+for angle in angles:
+    reward = 1 / angle
+    print(f"reward: {reward}, angle: {angle}")
